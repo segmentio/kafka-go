add28d3ba8b175f76abfbc000ef0f9adfd6ba965
Steve van Loben Sels <steve@segment.com>
2018-11-15 11:04:11 -0800

    added unit test for discardN, fixed an accounting bug in short read case

--- discard.go
+++ discard.go
@@ -3,14 +3,15 @@ package kafka
 import "bufio"
 
 func discardN(r *bufio.Reader, sz int, n int) (int, error) {
-	if n > sz {
-		n, err := r.Discard(sz)
+	var err error
+	if n <= sz {
+		n, err = r.Discard(n)
+	} else {
+		n, err = r.Discard(sz)
 		if err == nil {
 			err = errShortRead
 		}
-		return n, err
 	}
-	n, err := r.Discard(n)
 	return sz - n, err
 }
 
