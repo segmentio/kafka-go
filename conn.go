eca309bc170bab2ecf84474abac94b769dde6509
Anton Sadovnikov <scarbo1987@gmail.com>
2019-01-26 18:07:56 +0200

    issue-116: added method WriteCompressedMessagesAt (#171)
    * issue-116: added method WriteCompressedMessagesAt, that returns offset, partition, timestamp
    
    * issue-116: fixed after code review
    
    * issue-116: fixed after code review
    
    * issue-116: fixed convert to time.Time
    
    * issue-116: deleted extra code
    
    * issue-116: added dock to WriteCompressedMessagesAt method

--- conn.go
+++ conn.go
@@ -801,31 +801,48 @@ func (c *Conn) WriteMessages(msgs ...Message) (int, error) {
 // operation, it either fully succeeds or fails.
 //
 // If the compression codec is not nil, the messages will be compressed.
-func (c *Conn) WriteCompressedMessages(codec CompressionCodec, msgs ...Message) (int, error) {
+func (c *Conn) WriteCompressedMessages(codec CompressionCodec, msgs ...Message) (nbytes int, err error) {
+	nbytes, _, _, _, err = c.writeCompressedMessages(codec, msgs...)
+	return
+}
+
+// WriteCompressedMessages writes a batch of messages to the connection's topic
+// and partition, returning the number of bytes written, partition and offset numbers
+// and timestamp assigned by the kafka broker to the message set. The write is an atomic
+// operation, it either fully succeeds or fails.
+//
+// If the compression codec is not nil, the messages will be compressed.
+func (c *Conn) WriteCompressedMessagesAt(codec CompressionCodec, msgs ...Message) (nbytes int, partition int32, offset int64, appendTime time.Time, err error) {
+	return c.writeCompressedMessages(codec, msgs...)
+}
+
+func (c *Conn) writeCompressedMessages(codec CompressionCodec, msgs ...Message) (nbytes int, partition int32, offset int64, appendTime time.Time, err error) {
+
 	if len(msgs) == 0 {
-		return 0, nil
+		return
 	}
 
 	writeTime := time.Now()
-	n := 0
 	for i, msg := range msgs {
 		// users may believe they can set the Topic and/or Partition
 		// on the kafka message.
 		if msg.Topic != "" && msg.Topic != c.topic {
-			return 0, errInvalidWriteTopic
+			err = errInvalidWriteTopic
+			return
 		}
 		if msg.Partition != 0 {
-			return 0, errInvalidWritePartition
+			err = errInvalidWritePartition
+			return
 		}
 
 		if msg.Time.IsZero() {
 			msgs[i].Time = writeTime
 		}
 
-		n += len(msg.Key) + len(msg.Value)
+		nbytes += len(msg.Key) + len(msg.Value)
 	}
 
-	err := c.writeOperation(
+	err = c.writeOperation(
 		func(deadline time.Time, id int32) error {
 			now := time.Now()
 			deadline = adjustDeadlineForRTT(deadline, now, defaultRTT)
@@ -858,6 +875,12 @@ func (c *Conn) WriteCompressedMessages(codec CompressionCodec, msgs ...Message) 
 					if err == nil && p.ErrorCode != 0 {
 						err = Error(p.ErrorCode)
 					}
+					if err == nil {
+						partition = p.Partition
+						offset = p.Offset
+						appendTime = time.Unix(0, p.Timestamp*int64(time.Millisecond))
+					}
+
 					return size, err
 				})
 				if err != nil {
@@ -872,10 +895,10 @@ func (c *Conn) WriteCompressedMessages(codec CompressionCodec, msgs ...Message) 
 	)
 
 	if err != nil {
-		n = 0
+		nbytes = 0
 	}
 
-	return n, err
+	return
 }
 
 // SetRequiredAcks sets the number of acknowledges from replicas that the
