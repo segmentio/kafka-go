a05672a80997b36c45782343686a8e39583c6c43
Steve van Loben Sels <steve@segment.com>
2019-01-29 11:04:58 -0800

    Ensure reader leaves consumer group on graceul exit
    Issue #164

--- reader.go
+++ reader.go
@@ -1161,7 +1161,18 @@ func (r *Reader) Close() error {
 	if r.useConsumerGroup() {
 		// gracefully attempt to leave the consumer group on close
 		if generationID, membershipID := r.membership(); generationID > 0 && membershipID != "" {
-			if conn, err := r.coordinator(); err == nil {
+
+			// note that we do not call r.coordinator() because that passes
+			// r.stctx go DialContext.  we just cancelled that context a couple
+			// lines above by calling r.stop().
+			r.mutex.Lock()
+			address := r.address
+			r.mutex.Unlock()
+
+			// DialContext will wrap the background context to add appropriate
+			// timeout/deadline configuration.
+			conn, err := r.config.Dialer.DialContext(context.Background(), "tcp", address)
+			if err == nil {
 				_ = r.leaveGroup(conn)
 			}
 		}
