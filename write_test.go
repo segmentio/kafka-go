8355c57b1b5797e82814b845eafe6dc4ab5d9f43
Steve van Loben Sels <steve@segment.com>
2019-03-15 15:08:53 -0700

    Fix a go vet error thrown starting with go 1.12

--- write_test.go
+++ write_test.go
@@ -203,7 +203,9 @@ func TestWriteV2RecordBatch(t *testing.T) {
 		BatchSize: 5,
 	})
 
-	ctx, _ := context.WithTimeout(context.Background(), 5*time.Second)
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
 	if err := w.WriteMessages(ctx, msgs...); err != nil {
 		t.Errorf("Failed to write v2 messages to kafka: %v", err)
 		return
