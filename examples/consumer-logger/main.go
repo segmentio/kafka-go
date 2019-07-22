e31ffda5f6bd143cc41aab2d5051cf053f7aa6ef
Alan <zg.zhu@daocloud.io>
2019-07-22 14:25:57 +0800

    auto split kafkaURL to array (#280)
    auto split kafkaURL to array

--- examples/consumer-logger/main.go
+++ examples/consumer-logger/main.go
@@ -5,13 +5,15 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"strings"
 
 	kafka "github.com/segmentio/kafka-go"
 )
 
 func getKafkaReader(kafkaURL, topic, groupID string) *kafka.Reader {
+	brokers := strings.Split(kafkaURL, ",")
 	return kafka.NewReader(kafka.ReaderConfig{
-		Brokers:  []string{kafkaURL},
+		Brokers:  brokers,
 		GroupID:  groupID,
 		Topic:    topic,
 		MinBytes: 10e3, // 10KB
