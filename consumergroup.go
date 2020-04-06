segmentio/kafka-go

commit c0cc0f571206eff9184e2c5ead31a9939b6078cf

fix offset commit bug (#424)
Co-authored-by: liuzhiqing <liuzhiqing@taou.com>

consumergroup.go
@@ -50,7 +50,7 @@ const (
 
 	// defaultRetentionTime holds the length of time a the consumer group will be
 	// saved by kafka.  This value tells the broker to use its configured value.
-	defaultRetentionTime = -1
+	defaultRetentionTime = -1 * time.Millisecond
 
 	// defaultPartitionWatchTime contains the amount of time the kafka-go will wait to
 	// query the brokers looking for partition changes.

commit fae139e3fa3e44596603321ea86e592a815acef6

hot fix: fix unit test failure introduced by #424

consumergroup.go
@@ -215,7 +215,7 @@ func (config *ConsumerGroupConfig) Validate() error {
 		return errors.New(fmt.Sprintf("JoinGroupBackoff out of bounds: %d", config.JoinGroupBackoff))
 	}
 
-	if config.RetentionTime < defaultRetentionTime {
+	if config.RetentionTime < 0 && config.RetentionTime != defaultRetentionTime {
 		return errors.New(fmt.Sprintf("RetentionTime out of bounds: %d", config.RetentionTime))
 	}
 