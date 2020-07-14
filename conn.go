99faf011cac6c1414c298a5d9cb88b2d4ad3fc16
Benjamin Yolken <benjamin.yolken@segment.com>
2020-07-13 20:18:17 -0700

    Add public methods for list and describe groups

--- conn.go
+++ conn.go
@@ -323,6 +323,53 @@ func (c *Conn) describeGroups(request describeGroupsRequestV0) (describeGroupsRe
 	return response, nil
 }
 
+type GroupInfo struct {
+	GroupID string
+	Members []MemberInfo
+}
+
+type MemberInfo struct {
+	MemberID          string
+	ClientID          string
+	ClientHost        string
+	MemberMetadata    []byte
+	MemberAssignments []byte
+}
+
+func (c *Conn) DescribeGroupsPublic(groups ...string) ([]GroupInfo, error) {
+	req := describeGroupsRequestV0{
+		GroupIDs: groups,
+	}
+	resp, err := c.describeGroups(req)
+	if err != nil {
+		return nil, err
+	}
+	groupInfos := make([]GroupInfo, len(resp.Groups))
+
+	for _, group := range resp.Groups {
+		groupInfo := GroupInfo{
+			GroupID: group.GroupID,
+			Members: make([]MemberInfo, len(group.Members)),
+		}
+		for _, member := range group.Members {
+			groupInfo.Members = append(
+				groupInfo.Members,
+				MemberInfo{
+					MemberID:          member.MemberID,
+					ClientID:          member.ClientID,
+					ClientHost:        member.ClientHost,
+					MemberMetadata:    member.MemberMetadata,
+					MemberAssignments: member.MemberAssignments,
+				},
+			)
+		}
+
+		groupInfos = append(groupInfos, groupInfo)
+	}
+
+	return groupInfos, nil
+}
+
 // findCoordinator finds the coordinator for the specified group or transaction
 //
 // See http://kafka.apache.org/protocol.html#The_Messages_FindCoordinator
@@ -431,7 +478,7 @@ func (c *Conn) leaveGroup(request leaveGroupRequestV0) (leaveGroupResponseV0, er
 // listGroups lists all the consumer groups
 //
 // See http://kafka.apache.org/protocol.html#The_Messages_ListGroups
-func (c *Conn) ListGroups(request listGroupsRequestV1) (listGroupsResponseV1, error) {
+func (c *Conn) listGroups(request listGroupsRequestV1) (listGroupsResponseV1, error) {
 	var response listGroupsResponseV1
 
 	err := c.readOperation(
@@ -452,6 +499,18 @@ func (c *Conn) ListGroups(request listGroupsRequestV1) (listGroupsResponseV1, er
 	}
 
 	return response, nil
+}
+
+func (c *Conn) ListGroupsPublic() ([]string, error) {
+	resp, err := c.listGroups(listGroupsRequestV1{})
+	if err != nil {
+		return nil, err
+	}
+	groupIDs := make([]string, len(resp.Groups))
+	for _, group := range resp.Groups {
+		groupIDs = append(groupIDs, group.GroupID)
+	}
+	return groupIDs, nil
 }
 
 // offsetCommit commits the specified topic partition offsets
