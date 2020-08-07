19c234ac4a4e6a791b4796bf1d8e2c0d1555f9c3
Achille Roussel <achille@segment.com>
2020-08-06 18:41:13 -0700

    fix backward compatiblity with kafka-go v0.3.x

--- snappy/snappy.go
+++ snappy/snappy.go
@@ -2,3 +2,23 @@
 // breaking the majority of programs that imported it to install the compression
 // codec, which is now always included.
 package snappy
+
+import "github.com/segmentio/kafka-go/compress/snappy"
+
+type CompressionCodec = snappy.Codec
+
+type Framing = snappy.Framing
+
+const (
+	Code     = 2
+	Framed   = snappy.Framed
+	Unframed = snappy.Unframed
+)
+
+func NewCompressionCodec() *CompressionCodec {
+	return NewCompressionCodecFraming(Framed)
+}
+
+func NewCompressionCodecFraming(framing Framing) *CompressionCodec {
+	return &CompressionCodec{Framing: framing}
+}
