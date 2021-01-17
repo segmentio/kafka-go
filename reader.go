c549b2edef2842f223c7378d5cf5a51a74b4e609
iurii <SidorenkoYury@gmail.com>
2021-01-17 21:11:47 +0300

    Update reader.go
    Co-authored-by: Nicholas Sun <olassun2@gmail.com>

--- reader.go
+++ reader.go
@@ -1234,7 +1234,7 @@ func (r *reader) run(ctx context.Context, offset int64) {
 
 				if err != nil {
 					r.withErrorLogger(func(log Logger) {
-						log.Printf("the kafka reader got an error while attempting to determine whether it was reading before the first offset or after the last offset of partition %d of %s, err: %v", r.partition, r.topic, err)
+						log.Printf("the kafka reader got an error while attempting to determine whether it was reading before the first offset or after the last offset of partition %d of %s: %s", r.partition, r.topic, err)
 					})
 					conn.Close()
 					break readLoop
