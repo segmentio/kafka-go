798fc490bf1e92af4aa23d45341eceba956d3717
Nicholas Sun <olassun2@gmail.com>
2021-07-16 11:12:06 -0700

    Update initproducerid_test.go

--- initproducerid_test.go
+++ initproducerid_test.go
@@ -33,7 +33,7 @@ func TestClientInitProducerId(t *testing.T) {
 	}
 
 	// Now establish a connection with the transaction coordinator
-	transactionCoordinator := TCP(net.JoinHostPort(respc.Coordinator.Host, strconv.Itoa(respc.Coordinator.Port)))
+	transactionCoordinator := TCP(net.JoinHostPort(respc.Coordinator.Host, strconv.Itoa(int(respc.Coordinator.Port))))
 	client, shutdown = newClient(transactionCoordinator)
 	defer shutdown()
 
