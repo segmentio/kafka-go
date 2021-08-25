1fbd53bc1a503face36d4462b2cb00a94456ebb5
Matteo Rolla <matteo@rolla.io>
2021-08-25 19:09:44 +0200

    it has been a long day

--- transport.go
+++ transport.go
@@ -1161,10 +1161,10 @@ func (g *connGroup) connect(ctx context.Context, addr net.Addr) (*conn, error) {
 	pc.SetDeadline(time.Time{})
 
 	if g.pool.sasl != nil {
-		metadata := sasl.Metadata{
+		metadata := &sasl.Metadata{
 			BrokerAddress: netAddr.String(),
 		}
-		if err := authenticateSASL(sasl.WithMetadata(ctx, &metadata), pc, g.pool.sasl); err != nil {
+		if err := authenticateSASL(sasl.WithMetadata(ctx, metadata), pc, g.pool.sasl); err != nil {
 			return nil, err
 		}
 	}
