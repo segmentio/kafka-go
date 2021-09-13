7c2c02166dbce60f2705a6218426f8ba9ac29ec0
rhansen2 <rob.hansen@sendgrid.com>
2021-09-12 20:58:22 -0700

    Update tests to check all fields.

--- protocol/heartbeat/heartbeat_test.go
+++ protocol/heartbeat/heartbeat_test.go
@@ -8,25 +8,34 @@ import (
 )
 
 func TestHeartbeatRequest(t *testing.T) {
-	versions := []int16{
-		0, 1, 2, 3, 4,
-	}
-
-	for _, version := range versions {
+	// Versions 0-3 have all the same fields.
+	for _, version := range []int16{0, 1, 2, 3} {
 		prototest.TestRequest(t, version, &heartbeat.Request{
-			GroupID:      "test",
+			GroupID:      "group-1",
 			GenerationID: 1,
 			MemberID:     "member-1",
+		})
+	}
+
+	for _, version := range []int16{4} {
+		prototest.TestRequest(t, version, &heartbeat.Request{
+			GroupID:         "group-2",
+			GenerationID:    10,
+			MemberID:        "member-2",
+			GroupInstanceID: "instace-1",
 		})
 	}
 }
 
 func TestHeartbeatResponse(t *testing.T) {
-	versions := []int16{
-		0, 1, 2, 3, 4,
+	for _, version := range []int16{0} {
+		prototest.TestResponse(t, version, &heartbeat.Response{
+			ErrorCode: 4,
+		})
 	}
 
-	for _, version := range versions {
+	// Versions 1-4 have all the same fields.
+	for _, version := range []int16{1, 2, 3, 4} {
 		prototest.TestResponse(t, version, &heartbeat.Response{
 			ErrorCode:      4,
 			ThrottleTimeMs: 10,
