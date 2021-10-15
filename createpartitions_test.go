d8dd9b00cf48af7abc5144bbc574ccf03c3199a0
Rob Hansen <rob.hansen@sendgrid.com>
2021-10-15 14:40:18 -0700

    Makes Assignments nullable in createpartitions RequestTopic (#758)

--- createpartitions_test.go
+++ createpartitions_test.go
@@ -33,7 +33,36 @@ func TestClientCreatePartitions(t *testing.T) {
 		},
 		ValidateOnly: false,
 	})
+	if err != nil {
+		t.Fatal(err)
+	}
 
+	if err := res.Errors[topic]; err != nil {
+		t.Error(err)
+	}
+}
+
+func TestClientCreatePartitionsNoAssignments(t *testing.T) {
+	if !ktesting.KafkaIsAtLeast("1.0.1") {
+		return
+	}
+
+	client, shutdown := newLocalClient()
+	defer shutdown()
+
+	topic := makeTopic()
+	createTopic(t, topic, 1)
+	defer deleteTopic(t, topic)
+
+	res, err := client.CreatePartitions(context.Background(), &CreatePartitionsRequest{
+		Topics: []TopicPartitionsConfig{
+			{
+				Name:  topic,
+				Count: 2,
+			},
+		},
+		ValidateOnly: false,
+	})
 	if err != nil {
 		t.Fatal(err)
 	}
