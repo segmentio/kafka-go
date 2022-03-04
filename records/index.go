2f35e94ae3c96e646531c78ded7987630685f71c
Achille Roussel <achille@segment.com>
2022-03-03 17:02:10 -0800

    add records index and tests

--- records/index.go
+++ records/index.go
@@ -15,34 +15,76 @@ var (
 	errCommitted = errors.New("records index update was already committed")
 )
 
+// The Index interface represents an index (and reverse index) of Kafka message
+// keys to offsets.
+//
+// Index instances must be safe to use concurrently from multiple goroutines.
 type Index interface {
+	// Returns a selection of keys that match the given prefix.
+	//
+	// The returned Select object must be closed by the program when it is not
+	// needed anymore in order to release system resources allocated to hold
+	// the query results.
 	Select(ctx context.Context, prefix []byte) Select
 
+	// Performs an update on the index, returning an Update object which can be
+	// used apply mutations.
+	//
+	// The context passed to the method is captured by the returned Update and
+	// will be used to abort the transaction if it is canceled before applying
+	// the update.
+	//
+	// Updates are performed atomically on the index when committed.
+	//
+	// The returned Update object must be closed when the program does not need
+	// it anymore in order to release system resources allocated for the
+	// transaction.
 	Update(ctx context.Context) Update
 }
 
+// The Select interface is an iterator exposing the results of selecting a set
+// of keys by prefix on an index.
+//
+// Selections must be closed when the program has finished using the values.
+// If an error occured while iterating over the query results, it will be
+// returned by the call to Close.
 type Select interface {
 	io.Closer
 
+	// Next is called to advance the selection to the next position.
+	//
+	// Newly created selections are positioned before the first result, the Next
+	// method must then be called before the first call to Entry.
 	Next() bool
 
+	// Entry is called to return the pair of key and offset that the selection
+	// is currently positioned on. The returned key must be treated as an
+	// immutable value by the application, and remains valid until the next call
+	// to Next or Close.
 	Entry() (key []byte, offset int64)
 }
 
+// The Update interface represents a transaction that updates an index.
 type Update interface {
 	io.Closer
 
+	// Inserts a new entry into the index.
 	Insert(key []byte, offset int64) error
 
+	// Deletes a key from the index.
 	Delete(key []byte) error
 
+	// Drops a key prefix from the index.
 	DropKeys(prefix []byte) error
 
+	// Drops offsets up to the given limit from the index.
 	DropOffsets(limit int64) error
 
+	// Commits the update to the index.
 	Commit() error
 }
 
+// NewIndex constructs a new in-memory index.
 func NewIndex() Index { return new(index) }
 
 type index struct {
@@ -78,28 +120,39 @@ func (index *index) delete(key []byte, _ int64) {
 }
 
 func (index *index) dropKeys(prefix []byte, _ int64) {
+	keys := make([][]byte, 0, 42)
 	index.keys.Range(prefix, func(key []byte, offset int64) bool {
 		if !bytes.HasPrefix(key, prefix) {
 			return false
 		}
+		keys = append(keys, key)
 		index.offsets.Delete(offset)
 		return true
 	})
+	for _, key := range keys {
+		index.keys.Delete(key)
+	}
 }
 
 func (index *index) dropOffsets(_ []byte, limit int64) {
+	offsets := make([]int64, 0, 128)
 	index.offsets.Range(0, func(offset int64, key []byte) bool {
 		if offset >= limit {
 			return false
 		}
+		offsets = append(offsets, offset)
 		index.keys.Delete(key)
 		return true
 	})
+	for _, offset := range offsets {
+		index.offsets.Delete(offset)
+	}
 }
 
 func (index *index) Select(ctx context.Context, prefix []byte) Select {
 	slct := &indexSelect{
 		entries: make([]indexEntry, 0, 32),
+		index:   -1,
 	}
 
 	index.mutex.RLock()
