824250cd18002aefbbaeee4820fa6f251af23df7
Minha, Jeong <60059706+PzaThief@users.noreply.github.com>
2022-04-01 15:09:15 +0900

    Fixes tiny typo

--- record.go
+++ record.go
@@ -35,7 +35,7 @@ type Record = protocol.Record
 // RecordReader values are not safe to use concurrently from multiple goroutines.
 type RecordReader = protocol.RecordReader
 
-// NewRecordReade rconstructs a RecordSet which exposes the sequence of records
+// NewRecordReade reconstructs a RecordSet which exposes the sequence of records
 // passed as arguments.
 func NewRecordReader(records ...Record) RecordReader {
 	return protocol.NewRecordReader(records...)
