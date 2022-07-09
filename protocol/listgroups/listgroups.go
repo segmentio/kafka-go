9b289028ddbe9d20ab09848bca898e15e738209e
ksco.he <ksco.he@ponyft.com>
2022-07-10 02:12:05 +0800

    refactor(listgroups): remove unused func

--- protocol/listgroups/listgroups.go
+++ protocol/listgroups/listgroups.go
@@ -59,13 +59,18 @@ func (r *Response) Merge(requests []protocol.Message, results []interface{}) (
 	response := &Response{}
 
 	for r, result := range results {
-		brokerResp, err := Result(result)
+		brokerResp, err := protocol.Result(result)
 		if err != nil {
 			return nil, err
 		}
 
-		respGroups := make([]ResponseGroup, 0, len(brokerResp.Groups))
-		for _, brokerResp := range brokerResp.Groups {
+		resp, ok := brokerResp.(*Response)
+		if !ok {
+			return nil, fmt.Errorf("BUG: result must be a response but not %T", brokerResp)
+		}
+
+		respGroups := make([]ResponseGroup, 0, len(resp.Groups))
+		for _, brokerResp := range resp.Groups {
 			respGroups = append(
 				respGroups,
 				ResponseGroup{
@@ -80,20 +85,4 @@ func (r *Response) Merge(requests []protocol.Message, results []interface{}) (
 	}
 
 	return response, nil
-}
-
-// Result converts r to a Response or an error, or return a custom error if r could be
-// converted to these types.
-func Result(r interface{}) (Response, error) {
-	switch v := r.(type) {
-	case *Response:
-		if v == nil {
-			return Response{}, nil
-		}
-		return *v, nil
-	case error:
-		return Response{}, v
-	default:
-		return Response{}, fmt.Errorf("BUG: result must be a response or an error but not %T", v)
-	}
 }
