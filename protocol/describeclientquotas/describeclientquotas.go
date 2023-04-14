c7dd87c3158e2dab24a897d7c90793bd2ddc31c9
Peter Dannemann <pdannemann@twilio.com>
2023-04-14 16:30:48 -0400

    add descibeclientquotas protocol

--- protocol/describeclientquotas/describeclientquotas.go
+++ protocol/describeclientquotas/describeclientquotas.go
@@ -1,4 +1,4 @@
-package alterclientquotas
+package describeclientquotas
 
 import "github.com/segmentio/kafka-go/protocol"
 
@@ -11,30 +11,20 @@ type Request struct {
 	// messages.
 	_ struct{} `kafka:"min=v2,max=v2,tag"`
 
-	Entries      []Entry `kafka:"min=v0,max=v1"`
-	ValidateOnly boolean `kafka:"min=v0,max=v1"`
+	Components []Component `kafka:"min=v0,max=v1"`
+	Strict     boolean     `kafka:"min=v0,max=v1"`
 }
 
-func (r *Request) ApiKey() protocol.ApiKey { return protocol.AlterClientQuotas }
+func (r *Request) ApiKey() protocol.ApiKey { return protocol.DescribeClientQuotas }
 
 func (r *Request) Broker(cluster protocol.Cluster) (protocol.Broker, error) {
 	return cluster.Brokers[cluster.Controller], nil
 }
 
-type Entry struct {
-	Entities []Entity `kafka:"min=v0,max=v1"`
-	Ops      []Ops    `kafka:"min=v0,max=v1"`
-}
-
-type Entity struct {
+type Component struct {
 	EntityType string `kafka:"min=v0,max=v1"`
-	EntityName string `kafka:"min=v0,max=v1,nullable"`
-}
-
-type Ops struct {
-	Key    string  `kafka:"min=v0,max=v1"`
-	Value  float64 `kafka:"min=v0,max=v1"`
-	Remove boolean `kafka:"min=v0,max=v1"`
+	MatchType  int8   `kafka:"min=v0,max=v1"`
+	Match      string `kafka:"min=v0,max=v1,nullable"`
 }
 
 type Response struct {
@@ -43,15 +33,21 @@ type Response struct {
 	_ struct{} `kafka:"min=v2,max=v2,tag"`
 
 	ThrottleTimeMs int32            `kafka:"min=v0,max=v1"`
-	Results        []ResponseQuotas `kafka:"min=v0,max=v1"`
+	ErrorCode      int16            `kafka:"min=v0,max=v1"`
+	ErrorMessage   string           `kafka:"min=v0,max=v1,nullable"`
+	Entries        []ResponseQuotas `kafka:"min=v0,max=v1"`
 }
 
 func (r *Response) ApiKey() protocol.ApiKey { return protocol.CreateAcls }
 
+type Value struct {
+	Key   string  `kafka:"min=v0,max=v1"`
+	Value float64 `kafka:"min=v0,max=v1"`
+}
+
 type ResponseQuotas struct {
-	ErrorCode    int16    `kafka:"min=v0,max=v1"`
-	ErrorMessage string   `kafka:"min=v0,max=v1,nullable"`
-	Entities     []Entity `kafka:"min=v0,max=v1"`
+	Entities []Entity `kafka:"min=v0,max=v1"`
+	Values   []Value  `kafka:"min=v0,max=v1"`
 }
 
 var _ protocol.BrokerMessage = (*Request)(nil)
