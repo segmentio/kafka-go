904cbe4c496608107044af8595ee9bc01ce71793
Alireza Mortezaei <amortezaei@twilio.com>
2023-07-14 23:07:15 -0400

    some unit test fixes

--- offsetfetch_test.go
+++ offsetfetch_test.go
@@ -102,7 +102,7 @@ func TestOffsetFetchRequestWithNoTopic(t *testing.T) {
 func TestOffsetFetchRequestWithOneTopic(t *testing.T) {
 	ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
 	topic1 := makeTopic()
-	createTopic(t, topic1, 1)
+	createTopic(t, topic1, 3)
 	defer deleteTopic(t, topic1)
 	consumeGroup := makeGroupID()
 	numMsgs := 50
@@ -119,7 +119,8 @@ func TestOffsetFetchRequestWithOneTopic(t *testing.T) {
 	})
 	defer r1.Close()
 	prepareReader(t, ctx, r1, makeTestSequence(numMsgs)...)
-	client, topic2, shutdown := newLocalClientAndTopic()
+	topic2 := makeTopic()
+	client, shutdown := newLocalClientWithTopic(topic2, 3)
 	defer deleteTopic(t, topic2)
 	defer shutdown()
 	r2 := NewReader(ReaderConfig{
@@ -148,7 +149,7 @@ func TestOffsetFetchRequestWithOneTopic(t *testing.T) {
 	}
 
 	topicOffsets, err := client.OffsetFetch(ctx, &OffsetFetchRequest{GroupID: "cg", Topics: map[string][]int{
-		topic1: {0},
+		topic1: {1},
 	}})
 
 	if err != nil {
