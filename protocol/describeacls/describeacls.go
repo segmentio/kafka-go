5622f4f90ecdd9669f1e940435ebe34ce99b4481
Peter Dannemann <pdannemann@twilio.com>
2023-07-18 10:38:02 -0400

    protocol fixes

--- protocol/describeacls/describeacls.go
+++ protocol/describeacls/describeacls.go
@@ -22,10 +22,10 @@ func (r *Request) Broker(cluster protocol.Cluster) (protocol.Broker, error) {
 
 type ACLFilters struct {
 	ResourceTypeFilter        int8   `kafka:"min=v0,max=v3"`
-	ResourceNameFilter        string `kafka:"min=v0,max=v3,nullable"`
+	ResourceNameFilter        string `kafka:"min=v0,max=v1,nullable|min=v2,max=v3,nullable,compact"`
 	ResourcePatternTypeFilter int8   `kafka:"min=v1,max=v3"`
-	PrincipalFilter           string `kafka:"min=v0,max=v3,nullable"`
-	HostFilter                string `kafka:"min=v0,max=v3,nullable"`
+	PrincipalFilter           string `kafka:"min=v0,max=v1,nullable|min=v2,max=v3,nullable,compact"`
+	HostFilter                string `kafka:"min=v0,max=v1,nullable|min=v2,max=v3,nullable,compact"`
 	Operation                 int8   `kafka:"min=v0,max=v3"`
 	PermissionType            int8   `kafka:"min=v0,max=v3"`
 }
@@ -37,21 +37,30 @@ type Response struct {
 
 	ThrottleTimeMs int32      `kafka:"min=v0,max=v3"`
 	ErrorCode      int16      `kafka:"min=v0,max=v3"`
-	ErrorMessage   string     `kafka:"min=v0,max=v3,nullable"`
+	ErrorMessage   string     `kafka:"min=v0,max=v1,nullable|min=v2,max=v3,nullable,compact"`
 	Resources      []Resource `kafka:"min=v0,max=v3"`
 }
 
 func (r *Response) ApiKey() protocol.ApiKey { return protocol.DescribeAcls }
 
 type Resource struct {
+	// We need at least one tagged field to indicate that v2+ uses "flexible"
+	// messages.
+	_ struct{} `kafka:"min=v2,max=v3,tag"`
+
 	ResourceType int8          `kafka:"min=v0,max=v3"`
-	ResourceName string        `kafka:"min=v0,max=v3"`
+	ResourceName string        `kafka:"min=v0,max=v1|min=v2,max=v3,compact"`
+	PatternType  int8          `kafka:"min=v1,max=v3"`
 	ACLs         []ResponseACL `kafka:"min=v0,max=v3"`
 }
 
 type ResponseACL struct {
-	Principal      string `kafka:"min=v0,max=v3"`
-	Host           string `kafka:"min=v0,max=v3"`
+	// We need at least one tagged field to indicate that v2+ uses "flexible"
+	// messages.
+	_ struct{} `kafka:"min=v2,max=v3,tag"`
+
+	Principal      string `kafka:"min=v0,max=v1|min=v2,max=v3,compact"`
+	Host           string `kafka:"min=v0,max=v1|min=v2,max=v3,compact"`
 	Operation      int8   `kafka:"min=v0,max=v3"`
 	PermissionType int8   `kafka:"min=v0,max=v3"`
 }
