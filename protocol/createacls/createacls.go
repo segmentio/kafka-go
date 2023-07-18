ca11cb98637476fb0249cb74a68e41d4d5284f1a
Peter Dannemann <pdannemann@twilio.com>
2023-07-18 10:45:56 -0400

    fix createacls protocol

--- protocol/createacls/createacls.go
+++ protocol/createacls/createacls.go
@@ -22,10 +22,10 @@ func (r *Request) Broker(cluster protocol.Cluster) (protocol.Broker, error) {
 
 type RequestACLs struct {
 	ResourceType        int8   `kafka:"min=v0,max=v2"`
-	ResourceName        string `kafka:"min=v0,max=v2"`
-	ResourcePatternType int8   `kafka:"min=v0,max=v2"`
-	Principal           string `kafka:"min=v0,max=v2"`
-	Host                string `kafka:"min=v0,max=v2"`
+	ResourceName        string `kafka:"min=v0,max=v1|min=v2,max=v2,compact"`
+	ResourcePatternType int8   `kafka:"min=v1,max=v2"`
+	Principal           string `kafka:"min=v0,max=v1|min=v2,max=v2,compact"`
+	Host                string `kafka:"min=v0,max=v2|min=v2,max=v2,compact`
 	Operation           int8   `kafka:"min=v0,max=v2"`
 	PermissionType      int8   `kafka:"min=v0,max=v2"`
 }
@@ -43,7 +43,7 @@ func (r *Response) ApiKey() protocol.ApiKey { return protocol.CreateAcls }
 
 type ResponseACLs struct {
 	ErrorCode    int16  `kafka:"min=v0,max=v2"`
-	ErrorMessage string `kafka:"min=v0,max=v2,nullable"`
+	ErrorMessage string `kafka:"min=v0,max=v1,nullable|min=v2,max=v2,nullable,compact"`
 }
 
 var _ protocol.BrokerMessage = (*Request)(nil)
