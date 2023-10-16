5b97cf986aa9b7f5e1f855de6c24d7441a9f4acf
Boris Granveaud <bgranveaud@infovista.com>
2023-10-16 15:20:59 +0200

    Changed AlterPartitionReassignments request to support multiple topics (#1204)
    * changed AlterPartitionReassignments request to support multiple topics
    
    * keep compatibility with existing code.

--- alterpartitionreassignments_test.go
+++ alterpartitionreassignments_test.go
@@ -56,3 +56,64 @@ func TestClientAlterPartitionReassignments(t *testing.T) {
 		)
 	}
 }
+
+func TestClientAlterPartitionReassignmentsMultiTopics(t *testing.T) {
+	if !ktesting.KafkaIsAtLeast("2.4.0") {
+		return
+	}
+
+	ctx := context.Background()
+	client, shutdown := newLocalClient()
+	defer shutdown()
+
+	topic1 := makeTopic()
+	topic2 := makeTopic()
+	createTopic(t, topic1, 2)
+	createTopic(t, topic2, 2)
+	defer func() {
+		deleteTopic(t, topic1)
+		deleteTopic(t, topic2)
+	}()
+
+	// Local kafka only has 1 broker, so any partition reassignments are really no-ops.
+	resp, err := client.AlterPartitionReassignments(
+		ctx,
+		&AlterPartitionReassignmentsRequest{
+			Assignments: []AlterPartitionReassignmentsRequestAssignment{
+				{
+					Topic:       topic1,
+					PartitionID: 0,
+					BrokerIDs:   []int{1},
+				},
+				{
+					Topic:       topic1,
+					PartitionID: 1,
+					BrokerIDs:   []int{1},
+				},
+				{
+					Topic:       topic2,
+					PartitionID: 0,
+					BrokerIDs:   []int{1},
+				},
+			},
+		},
+	)
+
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resp.Error != nil {
+		t.Error(
+			"Unexpected error in response",
+			"expected", nil,
+			"got", resp.Error,
+		)
+	}
+	if len(resp.PartitionResults) != 3 {
+		t.Error(
+			"Unexpected length of partition results",
+			"expected", 3,
+			"got", len(resp.PartitionResults),
+		)
+	}
+}
