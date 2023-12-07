83ca7656a21c9b49f47f6fda339e665f4b1bdeb0
Kevin Burke <kevin.burke@segment.com>
2023-12-07 15:27:48 -0800

    logger: ensure compatibility with new log/slog package
    The API for the logger changes only slightly and should continue
    to be backwards compatible for any existing code.

--- logger.go
+++ logger.go
@@ -2,16 +2,19 @@ package kafka
 
 // Logger interface API for log.Logger.
 type Logger interface {
-	Printf(string, ...interface{})
+	Printf(string, ...any)
 }
 
-// LoggerFunc is a bridge between Logger and any third party logger
+// LoggerFunc is a bridge between Logger and any third party logger with the
+// same signature.
+//
 // Usage:
-//   l := NewLogger() // some logger
-//   r := kafka.NewReader(kafka.ReaderConfig{
-//     Logger:      kafka.LoggerFunc(l.Infof),
-//     ErrorLogger: kafka.LoggerFunc(l.Errorf),
-//   })
-type LoggerFunc func(string, ...interface{})
+//
+//	l := NewLogger() // some logger
+//	r := kafka.NewReader(kafka.ReaderConfig{
+//	  Logger:      kafka.LoggerFunc(l.Infof),
+//	  ErrorLogger: kafka.LoggerFunc(l.Errorf),
+//	})
+type LoggerFunc func(string, ...any)
 
-func (f LoggerFunc) Printf(msg string, args ...interface{}) { f(msg, args...) }
+func (f LoggerFunc) Printf(msg string, args ...any) { f(msg, args...) }
