3f3c325b6f0eb237cb77594c3f18643f7df54238
Peter Dannemann <pdannemann@twilio.com>
2024-01-04 14:22:14 -0500

    remove logger files

--- logger.go
+++ logger.go
@@ -2,19 +2,16 @@ package kafka
 
 // Logger interface API for log.Logger.
 type Logger interface {
-	Printf(string, ...any)
+	Printf(string, ...interface{})
 }
 
-// LoggerFunc is a bridge between Logger and any third party logger with the
-// same signature.
-//
+// LoggerFunc is a bridge between Logger and any third party logger
 // Usage:
-//
-//	l := NewLogger() // some logger
-//	r := kafka.NewReader(kafka.ReaderConfig{
-//	  Logger:      kafka.LoggerFunc(l.Infof),
-//	  ErrorLogger: kafka.LoggerFunc(l.Errorf),
-//	})
-type LoggerFunc func(string, ...any)
+//   l := NewLogger() // some logger
+//   r := kafka.NewReader(kafka.ReaderConfig{
+//     Logger:      kafka.LoggerFunc(l.Infof),
+//     ErrorLogger: kafka.LoggerFunc(l.Errorf),
+//   })
+type LoggerFunc func(string, ...interface{})
 
-func (f LoggerFunc) Printf(msg string, args ...any) { f(msg, args...) }
+func (f LoggerFunc) Printf(msg string, args ...interface{}) { f(msg, args...) }
